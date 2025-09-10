1321f3e9c4deba62c913825f434da42189d2e0db
Zheyuan Hu <huzheyuan428@gmail.com>
2025-09-09 23:36:37 -0400

    test plots

--- static/js/index.js
+++ static/js/index.js
@@ -65,13 +65,13 @@ $(document).ready(function() {
         player.currentTime = player.duration / 100 * this.value;
       })
     }, false);*/
-    preloadInterpolationImages();
+    // preloadInterpolationImages();
 
-    $('#interpolation-slider').on('input', function(event) {
-      setInterpolationImage(this.value);
-    });
-    setInterpolationImage(0);
-    $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
+    // $('#interpolation-slider').on('input', function(event) {
+    //   setInterpolationImage(this.value);
+    // });
+    // setInterpolationImage(0);
+    // $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
 
     bulmaSlider.attach();
 
@@ -88,11 +88,11 @@ function toggleAbstract() {
         // Collapse
         abstractFull.classList.remove('expanded');
         toggleButton.classList.remove('expanded');
-        toggleText.textContent = 'Read More';
+        toggleText.textContent = 'Click to Read More';
     } else {
         // Expand
         abstractFull.classList.add('expanded');
         toggleButton.classList.add('expanded');
-        toggleText.textContent = 'Read Less';
+        toggleText.textContent = 'Click to Read Less';
     }
 }
